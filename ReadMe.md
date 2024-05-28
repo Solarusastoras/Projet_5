77df3047f09b48aab1d00398c899156193c84927
Solarusastoras <micformweb@gmail.com>
2024-05-28 23:53:21 +0200

    Carouselle mise en place

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,22 @@
 # Print-It WebSite v.1
+
+Scénario
+
+Vous débutez comme développeur front-end en freelance, et venez de décrocher une offre pour dynamiser le site Internet statique d’une petite imprimerie familiale nommée Print it.
+
+Bannière du site print it
+
+Vous recevez votre premier mail de Lucien, le directeur :
+
+"Bonjour,
+
+J’espère que vous allez bien.
+
+Je vous envoie une vidéo pour vous expliquer en détail le carrousel idéal que je souhaite, ce sera plus simple :
+
+N’hésitez pas si vous avez des questions.
+
+Merci et bonne journée,
+Directeur @Print it"
+
+Muni des instructions et de la vidéo, vous entreprenez de réaliser le carrousel idéal !
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// images et Textes
+// images et Textes dans un tableau
 const slides = [
   {
     image: "./assets/images/slideshow/slide1.jpg",
@@ -23,27 +23,98 @@ const slides = [
 
 let IndexActuel = 0;
 
+function mettreAJourCarousel() {
+
+  // Image affichée
+  // Sélectionner l'élément image du carousel
+  let ElementImage = document.querySelector(".banner-img");
+
+  // Obtenir l'URL de l'image actuelle
+  let urlImageActuelle = slides[IndexActuel].image;
+
+  // Mettre à jour l'attribut 'src'
+  ElementImage.src = urlImageActuelle;
+
+
+  // Texte affichée
+  // Sélectionner l'élément de texte du carousel
+  let ElementTexte = document.querySelector("#banner p");
+
+  // Obtenir le texte actuelle
+  let texteActuel = slides[IndexActuel].tagLine;
+
+  // Mettre à jour le contenu HTML de l'élément de texte avec le texte actuel
+  ElementTexte.innerHTML = texteActuel;
+
+
+  // Dots
+  // Sélectionner tous les points du carousel
+  let dots = document.querySelectorAll(".dot");
+
+  // Parcourir chaque dots
+  dots.forEach((dot, index) => {
+    // Supprimer la classe 'dot_selected' du dots
+    dot.classList.remove("dot_selected");
+
+    // Si l'index dots correspond à l'index actuel
+    if (index === IndexActuel) {
+      // Ajouter la classe 'dot_selected' au point
+      dot.classList.add("dot_selected");
+    }
+  });
+}
+//-----–----------------------------------------------
 // Sélectionner les flèches gauche et droite
+
 // Flèche gauche
+// Mettre à jour le carousel
 let arrowLeft = document.querySelector(".arrow_left");
-arrowLeft.addEventListener ("click", function (){
-console.log("Flèche gauche cliquée");
+arrowLeft.addEventListener("click", function () {
+  // Si l'index actuel est supérieur à 0
+  if (IndexActuel > 0) {
+    // Diminuer l'index actuel
+    IndexActuel -= 1;
+  } else {
+    // Sinon, aller à la dernière diapositive
+    IndexActuel = slides.length - 1;
+  }
+  console.log("Flèche gauche cliquée");
+
+  // Mettre à jour le carousel
+  mettreAJourCarousel();
 });
 
 // Flèche droite
 let arrowRight = document.querySelector(".arrow_right");
-arrowRight.addEventListener ("click", function (){
-console.log("Flèche droite cliquée");
+arrowRight.addEventListener("click", function () {
+  // Si l'index actuel est inférieur à la longueur des slides moins 1
+  if (IndexActuel < slides.length - 1) {
+    // Augmenter l'index actuel
+    IndexActuel += 1;
+  } else {
+    // Sinon, aller à la première slide
+    IndexActuel = 0;
+  }
+
+  // Mettre à jour le carousel
+  mettreAJourCarousel();
+
+  console.log("Flèche droite cliquée");
 });
 
+//---------------------------------------------------
+// faire apparaitre Dots dans les slides
+let conteneurdots = document.querySelector(".dots");
 
-//Dots
-// faire apparaitre Dots sous les images
-let dotsContainer = document.querySelector(".dots");
+// Parcourir chaque slide
+slides.forEach((slide, index) => {
+  // creation un nouvel élément div
+  let dot = document.createElement("div");
+  dot.classList.add("dot");
 
-// creation un nouvel élément div
-let dot = document.createElement("div");
-dot.classList.add("dot");
+  // Définir l'index de la slide comme attribut de données de l'élément div
+  dot.dataset.index = index;
 
-// Ajouter l'élément div au conteneur des points
-dotsContainer.appendChild(dot);
+  // Ajouter le dot à conteneurdots
+  conteneurdots.appendChild(dot);
+});
